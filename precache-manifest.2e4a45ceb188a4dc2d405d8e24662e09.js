b3cfd018e563d76900a7b6cbb1b759ae37768bb6
darellwest <ewere.njoagwu@gmail.com>
2020-05-11 05:32:05 +0100

    Updates

---
+++ precache-manifest.2e4a45ceb188a4dc2d405d8e24662e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ae43eb6a8438e1cfb36ea5286cdf50",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b0a41c3d4c743008a8ef",
+    "url": "/monsters-rolodex/static/css/main.55f272da.chunk.css"
+  },
+  {
+    "revision": "eccb2620810445ce214e",
+    "url": "/monsters-rolodex/static/js/2.6f5ea732.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.6f5ea732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0a41c3d4c743008a8ef",
+    "url": "/monsters-rolodex/static/js/main.48f7707d.chunk.js"
+  },
+  {
+    "revision": "b9df087615a70e978ed1",
+    "url": "/monsters-rolodex/static/js/runtime-main.8e55d001.js"
+  }
+]);
